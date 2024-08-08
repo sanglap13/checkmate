fd73c3b84397c065cd23cdf993f56f65a1a47428
sanglap13 <sanglapmridha@gmail.com>
2024-08-09 00:14:29 +0530

    feat: add game page just layout

--- frontend/src/components/pages/Game.tsx
+++ frontend/src/components/pages/Game.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Button from "../shared/Button";
+import ChessBoard from "../shared/ChessBoard";
 
 const Game = () => {
-  return <div>Game</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="pt-8 max-w-screen-xl w-full">
+        <div className="grid grid-cols-6 gap-4 w-full">
+          <div className="col-span-4 bg-red-200 w-full">
+            <ChessBoard />
+          </div>
+          <div className="col-span-2 bg-blue-200 w-full">
+            <Button onClick={() => {}}>Play</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Game;
