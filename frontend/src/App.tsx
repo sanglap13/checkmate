902784c5f9008cce98f0e585736e8de200864fe4
sanglap13 <sanglapmridha@gmail.com>
2024-08-08 23:57:24 +0530

    feat: add home page design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,14 @@ import Game from "./components/pages/Game";
 
 const App = () => {
   return (
-    <>
+    <div className="h-screen bg-[#302e2b]">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game" element={<Game />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 };
 
--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,22 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import { ChessBoardImg } from "../../assets";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <div className="mt-2">
+    <div className="flex justify-center">
+      <div className="pt-8 max-w-screen-xl">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
           <div className="flex justify-center">
             <img src={ChessBoardImg} alt="chessboard" className="max-w-96" />
           </div>
-          <div>
-            <h1 className="text-4xl font-bold">
-              Play Chess online on the #2 site!
-            </h1>
-            <p className="mt-2 text-lg">
-              Play chess with your friends or with random people online.
-            </p>
-            <div className="mt-4">
-              <button className="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded">
+          <div className="pt-16">
+            <div className="flex justify-center">
+              <h1 className="text-4xl font-bold text-white">
+                Play Chess online on the #2 site!
+              </h1>
+            </div>
+            <div className="flex justify-center">
+              <p className="mt-2 text-lg text-slate-400">
+                Play chess with your friends or with random people online.
+              </p>
+            </div>
+            <div className="mt-8 flex justify-center">
+              <button
+                onClick={() => navigate("/game")}
+                className="px-8 py-6 text-2xl bg-[#80b64b] hover:bg-[#9fcf6e] text-white rounded font-bold"
+              >
                 Play Now
               </button>
             </div>
